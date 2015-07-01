b757b830a9b6190a906c732b446181bd7c321057
Rafael Soutelino <rsoutelino@gmail.com>
2015-07-01 20:48:49 -0300

    terminei o script para os dados do eduardo

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,7 @@
+import os
+import matplotlib.pyplot as plt
+
+
 def div(numerador=8, denominador=2):
     """
     Faz uma divisao simples
@@ -5,3 +9,32 @@ def div(numerador=8, denominador=2):
     denominador :    o denominador da fracao
     """
     return numerador/denominador
+
+
+def plot_profiles(cond, temp, salt, pres, filename, savedir):
+    """
+    Escrever docstring
+    INPUT: 
+        cond: blaa blaa
+    OUTPUT: 
+        graphs are genarated and seved at "savedir"
+    """
+    plt.figure(figsize=(10,5))
+    
+    plt.subplot(131)
+    plt.plot(temp, -pres)
+    plt.title('Temperature')
+
+    plt.subplot(132)
+    plt.plot(salt, -pres)
+    plt.title('Salinity')
+    plt.suptitle(filename.split('/')[-1])
+
+    plt.subplot(133)
+    plt.plot(cond, -pres)
+    plt.title('Conductivity')
+
+    figname = filename.split('/')[-1].replace('txt', 'png')
+    figname = os.path.join(savedir, figname)
+    print "    --> saving png"
+    plt.savefig(figname)
