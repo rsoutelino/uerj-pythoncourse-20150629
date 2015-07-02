5027dac70c1c2b21a365ef6ba8352b887fb08e70
Rafael Soutelino <rsoutelino@gmail.com>
2015-07-01 21:14:39 -0300

    pythonizei o script, aplicando a orientacao a objeto com o uso de classes

--- ctd_fernanda.py
+++ ctd_fernanda.py
@@ -5,39 +5,42 @@ import matplotlib.pyplot as plt
 from funcoes import *
 
 
-DATADIR = "/home/phellipe/Desktop/uerj-pythoncourse-20150629/sandbox/eduardorichard"
+DATADIR = "/home/phellipe/Desktop/uerj-pythoncourse-20150629/sandbox/eduardorichard/"
 NHEADERS = 2
 FIGDIR = "/home/phellipe/Desktop/figures"
 
 #===============================================================
 
-def read_header(lines, n=NHEADERS):
-    for k in range(n):
-        if 'TEMP' in lines[k]:
-            variables = lines[k].split()
-        if '(C)' in lines[k]:
-            units = lines[k].split()
+class Cast(object):
+    """docstring for Cast"""
+    def __init__(self, filename):
+        self.filename = filename
+        f = open(filename)
+        self.lines = f.readlines()
 
-    return variables, units
+    def read_header(self, n=NHEADERS):
+        for k in range(n):
+            if 'TEMP' in self.lines[k]:
+                self.variables = self.lines[k].split()
+            if '(C)' in self.lines[k]:
+                self.units = self.lines[k].split()
 
-def read_data(lines, n=NHEADERS):
-    cond, temp, pres, salt = [], [], [], []
+    def read_data(self, n=NHEADERS):
+        cond, temp, pres, salt = [], [], [], []
 
-    for line, count in zip(lines, range(len(lines))):
-        if count in range(n):
-            continue
+        for line, count in zip(self.lines, range(len(self.lines))):
+            if count in range(n):
+                continue
 
-        cond.append( float(line.split(',')[0] ) )
-        temp.append( float(line.split(',')[1] ) )
-        pres.append( float(line.split(',')[2] ) )
-        salt.append( float(line.split(',')[3] ) )
+            cond.append( float(line.split(',')[0] ) )
+            temp.append( float(line.split(',')[1] ) )
+            pres.append( float(line.split(',')[2] ) )
+            salt.append( float(line.split(',')[3] ) )
 
-    cond = np.array(cond)
-    temp = np.array(temp)
-    salt = np.array(salt)
-    pres = np.array(pres)
-
-    return cond, temp, salt, pres
+        self.cond = np.array(cond)
+        self.temp = np.array(temp)
+        self.salt = np.array(salt)
+        self.pres = np.array(pres)
 
 
 filelist = glob.glob( os.path.join(DATADIR, '*.txt') )
@@ -50,15 +53,11 @@ for filename in filelist:
     if "SED" in filename:
         filelist.remove(filename)
 
-
 for filename in filelist:
     print "Reading and plotting " + filename.split('/')[-1]
-
-    f = open(filename)
-    lines = f.readlines()
-
-    variables, units = read_header(lines)
-    cond, temp, salt, pres = read_data(lines)
-
-    plot_profiles(cond, temp, salt, pres, filename, FIGDIR)
+    estacao = Cast(filename)
+    estacao.read_header()
+    estacao.read_data()
+    plot_profiles(estacao.cond, estacao.temp, estacao.salt, 
+                  estacao.pres, filename, FIGDIR)
 
