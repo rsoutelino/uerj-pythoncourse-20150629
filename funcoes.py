bab1f664965582002337e791d423e0c7d60c59ea
Rafael Soutelino <rsoutelino@gmail.com>
2015-07-02 19:33:47 -0300

    deixamos as funcoes mais genericas para diferentes instrumentos

--- funcoes.py
+++ funcoes.py
@@ -11,7 +11,7 @@ def div(numerador=8, denominador=2):
     return numerador/denominador
 
 
-def plot_profiles(cond, temp, salt, pres, filename, savedir):
+def plot_profiles(temp, salt, pres, filename, savedir):
     """
     Escrever docstring
     INPUT: 
@@ -21,27 +21,18 @@ def plot_profiles(cond, temp, salt, pres, filename, savedir):
     """
     plt.figure(figsize=(10,5))
     
-    plt.subplot(131)
+    plt.subplot(121)
     plt.plot(temp, -pres)
     plt.title('Temperature')
 
-    plt.subplot(132)
+    plt.subplot(122)
     plt.plot(salt, -pres)
     plt.title('Salinity')
-    
-    if sys.platform == 'win32' or sys.platform == 'win64':
-        figname = filename.split('\\')[-1].replace('txt', 'png')
-    else:
-        figname = filename.split('/')[-1].replace('txt', 'png')
-
-    plt.subplot(133)
-    plt.plot(cond, -pres)
-    plt.title('Conductivity')
 
     if sys.platform == 'win32' or sys.platform == 'win64':
-        figname = filename.split('\\')[-1].replace('txt', 'png')
+        figname = filename.split('\\')[-1].split('.')[0] + '.png'
     else:
-        figname = filename.split('/')[-1].replace('txt', 'png')
+        figname = filename.split('/')[-1].split('.')[0] + '.png'
 
     figname = os.path.join(savedir, figname)
     print "    --> saving png"
