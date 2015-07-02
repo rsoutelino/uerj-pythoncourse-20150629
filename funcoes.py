e6855f9630517b70c790852f1c3b366e6d6201d4
Rafael Soutelino <rsoutelino@gmail.com>
2015-07-02 18:16:58 -0300

    adicionei suporte aos pathnames do windows com backslash

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import matplotlib.pyplot as plt
 
 
@@ -28,13 +28,21 @@ def plot_profiles(cond, temp, salt, pres, filename, savedir):
     plt.subplot(132)
     plt.plot(salt, -pres)
     plt.title('Salinity')
-    plt.suptitle(filename.split('/')[-1])
+    
+    if sys.platform == 'win32' or sys.platform == 'win64':
+        figname = filename.split('\\')[-1].replace('txt', 'png')
+    else:
+        figname = filename.split('/')[-1].replace('txt', 'png')
 
     plt.subplot(133)
     plt.plot(cond, -pres)
     plt.title('Conductivity')
 
-    figname = filename.split('/')[-1].replace('txt', 'png')
+    if sys.platform == 'win32' or sys.platform == 'win64':
+        figname = filename.split('\\')[-1].replace('txt', 'png')
+    else:
+        figname = filename.split('/')[-1].replace('txt', 'png')
+
     figname = os.path.join(savedir, figname)
     print "    --> saving png"
     plt.savefig(figname)
